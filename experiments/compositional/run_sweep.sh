a3771f2a191445433f04788618580d25925d343c
Tim Hanson <sideskate@gmail.com>
2026-05-11 18:03:08 -0700

    turned off cuda kernels for the time being..

--- experiments/compositional/run_sweep.sh
+++ experiments/compositional/run_sweep.sh
@@ -32,13 +32,13 @@ for attn in "${ATTN_TYPES[@]}"; do
 	for task in "${TASKS[@]}"; do
 		echo "Running: Attn=$attn | Task=$task | Log=$LOG_NAME | Replicate=$REPL"
 
-		python train_orig.py --bf16 \
+		python train_orig.py \
 			--batch-size "$BATCH_SIZE" \
 			--epochs "$EPOCHS" \
 			--attn "$attn" \
 			--task "$task" \
 			--log-name "$LOG_NAME" \
-			--repl "$REPL"
-
+			--repl "$REPL" \
+			--no-cuda-kernels # for now! FIXME
 	done
 done
