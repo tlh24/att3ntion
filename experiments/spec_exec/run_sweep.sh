e54cc01e91f665082db493dbb7174e11c8b93406
Tim Hanson <sideskate@gmail.com>
2026-06-30 16:07:15 -0800

    missed the run sweep script

---
+++ experiments/spec_exec/run_sweep.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+parallel -j 16 -u 'python train.py --batch-size 16 --nsamp {1} --repl {2} --device cuda:$(( ({%} - 1) % 8 ))' ::: 0 1 2 4 8 16 ::: 1 2 3 4
+# {%} - 1 converts the slot (1-indexed) to the GPU number, so that no two runs are simultaneously executing on the same GPU.
