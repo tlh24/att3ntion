c0c3451c07f50baf78396aeed6ab1c919c097285
Tim Hanson <sideskate@gmail.com>
2026-07-02 14:58:00 -0800

    run sweep changes

--- experiments/spec_exec/run_sweep.sh
+++ experiments/spec_exec/run_sweep.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
-parallel -j 16 -u 'python train.py --batch-size 16 --epochs 30 --log-name testy --nsamp {1} --repl {2} --device cuda:$(( ({%} - 1) % 8 ))' ::: 0 1 2 4 8 16 32 ::: 1 2 3 4
+# parallel -j 16 -u 'python train.py --batch-size 16 --epochs 30 --log-name testy --nsamp {1} --repl {2} --device cuda:$(( ({%} - 1) % 8 ))' ::: 0 1 2 4 8 16 32 ::: 1 2 3 4
+
+parallel -j 16 -u 'python train.py --batch-size 16 --epochs 30 --log-name add --repl {1} --attn {2} --device cuda:$(( ({%} - 1) % 4 ))' ::: 1 2 3 4 5 6 7 8 ::: graph hypergraph
 # {%} - 1 converts the slot (1-indexed) to the GPU number, so that no two runs are simultaneously executing on the same GPU.
